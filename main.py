ffe680217d8c7853161b7df0744e88a6e18f9f17
ztjhz <59118459+ztjhz@users.noreply.github.com>
2021-10-16 09:30:24 +0800

    read dataset function

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ from bs4 import BeautifulSoup
 import requests # library to handle requests
 import json # library to handle JSON files
 from pandas.io.json import json_normalize # tranform JSON file into a pandas dataframe
+
+
+def main():
+    df = read_data_set("Groceries_dataset.csv")
+    process_data(df)
+
+def process_data(df):
+    df = df.sort_values(by="Date")
+    print(df)
+
+def read_data_set(file_name):
+    df = pd.read_csv(file_name)
+    return df
+
+main()
