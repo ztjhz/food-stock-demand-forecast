567d13bd2339a5f2b17ed8c341a1b22af447cb05
ztjhz <59118459+ztjhz@users.noreply.github.com>
2021-10-16 10:12:31 +0800

    expand dataset

--- main.py
+++ main.py
@@ -9,20 +9,34 @@ import datetime
 
 
 def main():
-    df = read_data_set("Groceries_dataset.csv")
+    df = load_data_set()
     process_data(df)
+
+def load_data_set():
+    df = read_data_set("Groceries_dataset.csv")
+    for i in range(5):
+        df2 = read_data_set("Groceries_dataset.csv")
+        df = df.append(df2)
+    return df
 
 def process_data(df):
     # sort df by date and drop unused columns
     # df = df.sort_values(by="Date")
     df = df.drop(columns=['Member_number'], axis=1)
     
-    # start_date = df["Date"].iloc[0]
-    # print(start_date)
-    valid_df = df.groupby(by="itemDescription").count().sort_values(by="Date")
-    valid_df = valid_df.drop(valid_df[valid_df.Date < 1000].index)
-    print(valid_df.count())
+   
+    # get items with many data points
+    valid_items_df = df.groupby(by="itemDescription").count().sort_values(by="Date")
+    valid_items_df = valid_items_df.drop(valid_items_df[valid_items_df.Date < 5000].index)
     
+    for item in valid_items_df.index:
+        temp_df = df[df.itemDescription == item]
+        # temp_df = temp_df.sort_values(by="Date")
+        temp_df["Date"] = pd.to_datetime(temp_df["Date"])
+        temp_df = temp_df.sort_values(by="Date")
+        # temp_df = pd.to_datetime(temp_df["Date"])
+        print(temp_df.tail())
+        print()
 
 def read_data_set(file_name):
     df = pd.read_csv(file_name)
